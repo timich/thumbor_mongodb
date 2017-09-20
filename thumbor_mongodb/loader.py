6d276915aabc7f8bebf58537631fb29cc8852f94
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-20 11:35:00 +0700

    User with work tc_thumbor

--- thumbor_mongodb/loader.py
+++ thumbor_mongodb/loader.py
@@ -7,24 +7,29 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 
-from tornado.concurrent import return_future
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 import gridfs
+from pymongo import MongoClient
+from tornado.concurrent import return_future
+
 
 def __conn__(self):
-	connection = MongoClient(
-		self.config.MONGO_STORAGE_SERVER_HOST,
-		self.config.MONGO_STORAGE_SERVER_PORT,
-		readPreference=self.config.MONGO_STORAGE_READ_PREFERENCE
-	)
-
-	db = connection[self.config.MONGO_STORAGE_SERVER_DB]
-	return db
+    connection = MongoClient(
+        self.config.MONGO_STORAGE_SERVER_HOST,
+        self.config.MONGO_STORAGE_SERVER_PORT,
+        readPreference='primaryPreferred'
+    )
+    db = connection[self.config.MONGO_STORAGE_SERVER_DB]
+    storage = db[self.config.MONGO_STORAGE_SERVER_COLLECTION]
+    return connection, db, storage
 
 @return_future
 def load(self, path, callback):
-	db = __conn__(self)
-	fs = gridfs.GridFS(db)
-	contents = fs.get(ObjectId(path)).read()
-	callback(contents)
+    connection, db, storage = __conn__(self)
+    stored = storage.find_one({'path': path})
+    if not stored:
+        callback(None)
+        return
+    fs = gridfs.GridFS(db)
+    contents = fs.get(stored['file_id']).read()
+    callback(str(contents))
+
