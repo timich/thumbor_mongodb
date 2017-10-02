3593c1ee83cbebf299edbda55ecf4a149785cbdc
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-10-02 11:11:43 +0700

    issuse Bug expired

--- thumbor_mongodb/loader.py
+++ thumbor_mongodb/loader.py
@@ -29,7 +29,7 @@ def load(self, path, callback):
     stored = storage.find_one({'path': path})
 
 
-    if not stored or self.__is_expired(stored):
+    if not stored or __is_expired(stored):
         callback(None)
         return
     fs = gridfs.GridFS(db)
